766afcefe96b8c96dc29d876943dc8260c6e8a3e
Dhruv <thedhruvsharma@gmail.com>
2023-10-12 18:49:14 +0530

    first commit

---
+++ lib/pages/map_pages.dart
@@ -0,0 +1,37 @@
+import 'package:ambuvians/widgets/panel_build.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+class Map_page extends StatefulWidget {
+  const Map_page({super.key});
+
+  @override
+  State<Map_page> createState() => _Map_pageState();
+}
+
+class _Map_pageState extends State<Map_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SlidingUpPanel(
+        panelBuilder: (sc) => PanelWidget(scrollController: sc),
+        body: Container(
+          child: FlutterMap(
+            options: MapOptions(backgroundColor: Colors.black),
+            children: [
+              TileLayer(
+                minZoom: 1,
+                maxZoom: 18,
+                backgroundColor: Colors.black,
+                urlTemplate:
+                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                subdomains: const ['a', 'b', 'c'],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
